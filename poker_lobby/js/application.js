56ae6646168ecab4d67a016ba3dd8c7f688ad907
Dmitry Vislov <vdv73rus@gmail.com>
2015-02-04 08:46:24 +0300

    Remove unused project. New added.

--- poker_lobby/js/application.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  $(document).ready(function() {
-    return $('.js-filter-button').click(function() {
-      $(this).toggleClass('nav-link__active');
-      return $('.js-filter-content').toggle();
-    });
-  });
-
-}).call(this);
