a5aebe64c9e9057658784445dd83e21fcd020940
Dmitry Vislov <vdv73rus@gmail.com>
2015-02-06 10:09:08 +0300

    Next step for marble-shop.

--- marble-shop/js/application.js
+++ marble-shop/js/application.js
@@ -1,4 +1,20 @@
 (function() {
-  $(document).ready(function() {});
+  $(document).ready(function() {
+    $("[data-action='change-form-edit-state']").click(function() {
+      $("[data-object='vcard-form']").toggleClass('editable');
+      if ($('.vcard-input').attr('readonly')) {
+        return $('.vcard-input').removeAttr('readonly');
+      } else {
+        return $('.vcard-input').attr('readonly', 'readonly');
+      }
+    });
+    $('[data-action="clear-self-input-value"]').click(function() {
+      $(this).prev('.vcard-input').attr('value', '');
+      return $(this).prev('.vcard-textarea').val('');
+    });
+    return $('[data-action="change-form-open-state"]').click(function() {
+      return $('.vcard-editor').slideToggle();
+    });
+  });
 
 }).call(this);
