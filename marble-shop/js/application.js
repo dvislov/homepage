06f95f943e203cf8ddda5b92bbefcf9bc93b7efa
Dmitry Vislov <vdv73rus@gmail.com>
2015-02-23 17:15:38 +0300

    Probably last changes.

--- marble-shop/js/application.js
+++ marble-shop/js/application.js
@@ -8,30 +8,38 @@
         $(".vcard-form input[type='submit']").attr('disabled', 'disabled');
       }
       $('.vcard-name').focus();
-      if ($('.vcard-input, .vcard-textarea').attr('readonly')) {
-        $('.vcard-input, .vcard-textarea').removeAttr('readonly');
+      if ($('.vcard-site').val() === "") {
+        $('.vcard-site').val("www.marble-x.ru");
+      }
+      if ($('.vcard-input').attr('readonly')) {
+        $('.vcard-input').removeAttr('readonly');
       } else {
-        $('.vcard-input, .vcard-textarea').attr('readonly', 'readonly');
+        $('.vcard-input').attr('readonly', 'readonly');
       }
       if ($("[data-object='vcard-form']").hasClass('editable')) {
-        return $('.vcard-input, .vcard-textarea').parents('.control-group').show();
+        return $('.vcard-input').parents('.control-group').show();
       } else {
-        $('.vcard-input').each(function() {
+        return $('.vcard-input').each(function() {
           if ($(this).val() === "") {
             return $(this).parents('.control-group').hide();
           }
         });
-        if ($('.vcard-textarea').val() === "") {
-          return $('.vcard-textarea').parents('.control-group').hide();
-        }
       }
     });
     $('[data-action="clear-self-input-value"]').click(function() {
-      $(this).prev('.vcard-input').attr('value', '');
-      return $(this).prev('.vcard-textarea').val('');
+      if ($(this).hasClass('controls-remove-address')) {
+        return $('.vcard-address-line1, .vcard-address-line2').val("");
+      } else {
+        return $(this).prev('.vcard-input').val("");
+      }
     });
     return $('[data-action="change-form-open-state"]').click(function() {
-      return $('.vcard-editor').slideToggle();
+      $('.vcard-editor').slideToggle();
+      if ($(".btn-form-state").attr('disabled')) {
+        return $(".btn-form-state").removeAttr('disabled');
+      } else {
+        return $(".btn-form-state").attr('disabled', 'disabled');
+      }
     });
   });
 
